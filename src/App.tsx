ec5278a9b3235f8d15e065076474455508c0f527
Idris <idrist2013@gmail.com>
2020-08-25 11:32:33 +0100

    created  header and styled the a tags

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Homepage from "./pages/index";
 import { Route, Switch } from "react-router-dom";
 import "./App.scss";
 import ShopPage from "./pages/ShopPage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path="/" exact component={Homepage} />
         <Route path="/shop" exact component={ShopPage} />
